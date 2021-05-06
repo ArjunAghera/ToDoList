f0d69a1ff234487f30ea2390cf8f0402aa77086e
ArjunAghera <agheraarjun23@gmail.com>
2021-05-06 11:04:45 +0530

    port changes

--- app.js
+++ app.js
@@ -126,11 +126,6 @@ app.post("/delete",function(req,res){
   }
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
